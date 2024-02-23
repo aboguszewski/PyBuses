5874529db2365dfea9e6733a7a304393b0af2cfe
aboguszewski <149703361+aboguszewski@users.noreply.github.com>
2024-02-23 07:52:58 +0100

    create setup.py

---
+++ buses/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name="buses", 
+    author="Adrian",
+    description="Pakiet z modułem do pobierania i analizowania danych dotyczących autobusów",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
